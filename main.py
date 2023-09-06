c8acdfd41a8ed8c27e3ada7f3abcf9ee0bc4921e
MariamMo404 <mariammohammed8702@gmail.com>
2023-09-06 20:16:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-# write your code here
+# write your code here 
+person = {
+           "name":"mariam" ,
+            "age" : 16 ,
+            "hobbies" :["yoga","art","travel","markting"]  
+         }
+print(person["name"])
+print(person["age"])
+person["age"] = 17
+person["country"] = "Egypt"
+print(person)
+print(len(person))
+person["hobbies"].append("reading")
+print(person)
+def check_hobbies(person):
+    if len(person["hobbies"])>=3 :
+      print("Wow you are amazing")
+    else:
+       print("error")
+check_hobbies(person)
